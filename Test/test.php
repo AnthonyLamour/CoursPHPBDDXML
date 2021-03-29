a63a2414f989bbd8f9d1f4bd23f360c5300f17b6
AnthonyLamour <a.lamour@ludus-academie.com>
2021-03-29 18:29:21 +0200

    ajout test htaccess

---
+++ Test/test.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Test</title>
+</head>
+<body>
+    <h1>Coucou</h1>
+    
+    <?php
+        echo $_POST["id"];
+        echo $_POST["Name"];
+    ?>
+
+</body>
+</html>
